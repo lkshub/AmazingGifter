39ab73959444f3f3576fc68222209d942de317d6
Kai, Lu <kailu@gatech.edu>
2016-06-23 22:20:31 -0400

    set up firebase and cocoapods

--- AmazingGifters/AppDelegate.swift
+++ AmazingGifters/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure()
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
---
+++ Pods/Target Support Files/Pods-AmazingGifters/Pods-AmazingGifters-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AmazingGifters : NSObject
+@end
+@implementation PodsDummy_Pods_AmazingGifters
+@end
