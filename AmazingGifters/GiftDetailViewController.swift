760eba4b71433fc013e809af6c56dec026e41691
Kai, Lu <kailu@gatech.edu>
2016-06-27 09:19:38 -0400

    restructure the project and make cancel btn work
    add a new “Views” group, the cancel button is on the Gift details view

---
+++ AmazingGifters/GiftDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GiftDetailViewController.swift
+//  AmazingGifters
+//
+//  Created by 陆恺 on 6/27/16.
+//  Copyright © 2016 Kai Lu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class GiftDetailViewController: UIViewController {
+    
+    @IBAction func btnCancel(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
+}
